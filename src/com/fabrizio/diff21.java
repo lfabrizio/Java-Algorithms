eaf62ad4126ab999be4900458c9685fa814e8a6e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-07 17:33:06 -0400

    diff21

---
+++ src/com/fabrizio/diff21.java
@@ -0,0 +1,10 @@
+package com.fabrizio;
+
+public class diff21 {
+    public int diff21(int n) {
+        if ( n <= 21) {
+            return 21 -n;
+        }
+        return (n - 21) * 2;
+    }
+}
