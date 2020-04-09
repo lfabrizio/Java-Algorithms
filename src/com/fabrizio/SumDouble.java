37c82d14a9b2ef1479c3739ae43e02d611cd2bae
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-09 17:17:44 -0400

    sumdouble

---
+++ src/com/fabrizio/SumDouble.java
@@ -0,0 +1,15 @@
+package com.fabrizio;
+// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+//         sumDouble(1, 2) → 3
+//         sumDouble(3, 2) → 5
+//         sumDouble(2, 2) → 8
+public class SumDouble {
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if ( a ==  b){
+            return sum * 2;
+        } else {
+            return sum;
+        }
+    }
+}
