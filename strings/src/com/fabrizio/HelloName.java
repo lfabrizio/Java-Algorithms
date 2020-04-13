c1dff20533b83f05ec97a7a82979a41b36e049d6
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-13 18:54:07 -0400

    hello name

---
+++ strings/src/com/fabrizio/HelloName.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+
+//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+public class HelloName {
+    public String helloName(String name) {
+        return "Hello " + name + "!" ;
+    }
+}
