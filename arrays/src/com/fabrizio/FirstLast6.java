c1de9713386d2174da31c82e32e5a95936a00d00
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-14 18:49:41 -0400

    firstlast 6 solved

---
+++ arrays/src/com/fabrizio/FirstLast6.java
@@ -0,0 +1,17 @@
+package com.fabrizio;
+
+//Given an array of ints,
+//        return true if 6 appears as either the first or last element in the array.
+//        The array will be length 1 or more.
+public class FirstLast6 {
+    public boolean firstLast6(int[] nums) {
+        if(nums[0] == 6) {
+            return true;
+        } else if (nums[nums.length-1] == 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
