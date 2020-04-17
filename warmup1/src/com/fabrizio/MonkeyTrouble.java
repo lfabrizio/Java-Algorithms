3b187f440c674c2c3bb212e2867ad833024f0bd8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-17 16:30:03 -0400

    monkey trouble

---
+++ warmup1/src/com/fabrizio/MonkeyTrouble.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if((aSmile && bSmile) || (!aSmile && !bSmile)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
