edbb873caf507a505a0af213c9a99ba361043c48
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-21 17:39:35 -0400

    in1020

---
+++ warmup1/src/com/fabrizio/In1020.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class In1020 {
+    public boolean in1020(int a, int b) {
+        if(a <= 20 && a >= 10 || b <= 20 && b >= 10){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
