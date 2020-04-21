f85e7ed5a798a2cce5d9a9754c474f3c6890573e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-21 18:13:48 -0400

    hasteen

---
+++ warmup1/src/com/fabrizio/HasTeen.java
@@ -0,0 +1,10 @@
+package com.fabrizio;
+
+public class HasTeen {
+      if(a <= 19 && a >= 13 || b <= 19 && b >= 13 || c <= 19 && c >=13){
+        return true;
+    } else {
+        return false;
+    }
+}
+}
