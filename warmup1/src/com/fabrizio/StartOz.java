fe4a938697b4057f5bab95a1452d93b50022e992
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-24 17:41:24 -0400

    startoz

---
+++ warmup1/src/com/fabrizio/StartOz.java
@@ -0,0 +1,7 @@
+package com.fabrizio;
+//Given a string, return a string made of the first 2 chars (if present),
+//        however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+public class StartOz {
+
+    
+}
