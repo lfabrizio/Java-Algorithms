a0e3e89384a8d8086a18d07ed6f0bed35f412d9e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-24 17:40:01 -0400

    MixStart

---
+++ warmup1/src/com/fabrizio/MixStart.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+//Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+public class MixStart {
+    public boolean mixStart(String str) {
+        if (str.length() < 3) {
+            return false;
+        }
+
+    }
+}
