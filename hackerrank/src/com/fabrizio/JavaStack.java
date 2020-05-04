323e43d7769c9a2a52807fc03ad6c32492165ed1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-04 19:31:31 -0400

    Java stack attempt

---
+++ hackerrank/src/com/fabrizio/JavaStack.java
@@ -0,0 +1,30 @@
+package com.fabrizio;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class JavaStack {
+
+        public static void main(String []argh)
+        {
+            Scanner sc = new Scanner(System.in);
+
+            // while (sc.hasNext()) {
+            // 	String input=sc.next();
+            // LIFO
+            //balance the brackets
+            // if statement to check open (, { , [
+//            // if match pop off the stack
+//            // if stack is empty = no
+//            // boolean - true is matched , false if not
+//
+//            stack<Character> = new Stack();
+//            // loop through the string length
+//            for (int i = 0; i < sc.length(); i++){
+//                if(sc.charAt(i) == '(' || sc.charAt == '{' || '[')
+            }
+
+
+        }
+
+
