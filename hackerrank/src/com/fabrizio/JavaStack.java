41eafb0eed60d6114b5e5b15d131a7d1cbd717ee
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-05 17:23:48 -0400

    saved progress

--- hackerrank/src/com/fabrizio/JavaStack.java
+++ hackerrank/src/com/fabrizio/JavaStack.java
@@ -9,21 +9,18 @@ public class JavaStack {
         {
             Scanner sc = new Scanner(System.in);
 
-            // while (sc.hasNext()) {
-            // 	String input=sc.next();
-            // LIFO
-            //balance the brackets
-            // if statement to check open (, { , [
-//            // if match pop off the stack
-//            // if stack is empty = no
-//            // boolean - true is matched , false if not
+             while (sc.hasNext()) {
+             	String input=sc.next();
+                List<String> brackets = new ArrayList<>(Arrays.asList(input.sc));
+                brackets.remove(0);
+                Stack<String> stack = new Stack<String>();
 //
 //            stack<Character> = new Stack();
 //            // loop through the string length
 //            for (int i = 0; i < sc.length(); i++){
 //                if(sc.charAt(i) == '(' || sc.charAt == '{' || '[')
             }
-
+// unfinished
 
         }
 
