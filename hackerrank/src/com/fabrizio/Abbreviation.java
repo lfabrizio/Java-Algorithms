b2befd4d2925eaa1d5fbd0e21646374c0df4dce1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-08 16:21:25 -0400

    abbreviation

---
+++ hackerrank/src/com/fabrizio/Abbreviation.java
@@ -0,0 +1,9 @@
+package com.fabrizio;
+//Complete the function  in the editor below. It must return either  or .
+//
+//        abbreviation has the following parameter(s):
+//
+//        a: the string to modify
+//        b: the string to match
+public class Abbreviation {
+}
