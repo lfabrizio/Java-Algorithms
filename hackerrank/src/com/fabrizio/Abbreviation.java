d843e38150ac32483f07e559354400ba3b35f77f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-08 16:23:39 -0400

    challenge

--- hackerrank/src/com/fabrizio/Abbreviation.java
+++ hackerrank/src/com/fabrizio/Abbreviation.java
@@ -6,4 +6,19 @@ package com.fabrizio;
 //        a: the string to modify
 //        b: the string to match
 public class Abbreviation {
+    public class Solution {
+    }
+
+    // Complete the abbreviation function below.
+    static String abbreviation(String a, String b) {
+// delete
+// return yes if equal
+// eliminate lowercase one by one
+        for(int i = 0; i < a.length(); i++){
+            for (int j =0; j < b.length(); j++){
+                if(a.charAt(i) == b.charAt(j)){
+                    return "YES";
+                }
+
+            }
 }
