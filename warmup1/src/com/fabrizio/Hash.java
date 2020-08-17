cb2fca3a64f0e757f60c89f19ed93a851b4a0570
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-17 18:22:55 -0400

    algo

---
+++ warmup1/src/com/fabrizio/Hash.java
@@ -0,0 +1,27 @@
+package com.fabrizio;
+
+import java.util.Arrays;
+
+public class Hash {
+
+    String[] theArray;
+    int arraySize;
+    int itemsInArray = 0;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void hashFunction(String[] stringsForArray, String[] theArray){
+        
+    }
+}
+
+    HashFunction(int size) {
+
+        arraySize = size;
+        theArray = new String[size];
+        Arrays.fill(theArray, "-1");
+    }
+
+
