1a103d632d056dafa4084d2d7d9bcd8049d36a83
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-18 17:43:57 -0400

    intmax

--- warmup1/src/com/fabrizio/Hash.java
+++ warmup1/src/com/fabrizio/Hash.java
@@ -1,31 +1,34 @@
-package com.fabrizio;
-
-import java.util.Arrays;
-
-public class Hash {
-
-    String[] theArray;
-    int arraySize;
-    int itemsInArray = 0;
-
-    public static void main(String[] args) {
-
-    }
-
-    public void hashFunction(String[] stringsForArray, String[] theArray){
-        for(int n = 0; n < stringsForArray.length; n++)
-
-            String newElementVal = stringsForArray[n];
-
-        theArray[Integer.parseInt(newElementVal)] = newElementVal;
-    }
-}
-
-    HashFunction(int size) {
-s
-        arraySize = size;
-        theArray = new String[size];
-        Arrays.fill(theArray, "-1");
-    }
+static int luckBalance(int k, int[][] contests) {
+        System.out.println(k);
+        List<Integer> luck = new ArrayList<Integer>();
+        int sum = 0;
+        for (int[] contest : contests) {
+        int i = contest[0];
+        int j = contest[1];
+        System.out.println(i + " " + j);
+        if (j == 1) {
+        luck.add(i);
+        }
+        sum += i;
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Luck items");
+        Collections.sort(luck);
+        for (int luckItem : luck) {
+        System.out.println(luckItem);
+        }
+        for (int i = k; i > 0; i--) {
+        luck.remove(i);
+        }
+        System.out.println("After removal");
+        for (int luckItem : luck) {
+        System.out.println(luckItem);
+        }
+        for (int luckItem : luck) {
+        sum = sum - (luckItem * 2);
+        }
+        System.out.println("New sum: " + sum);
+        return sum;
+        }
 
 
