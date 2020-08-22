67b7121b63502535f6e131486a0e732f4abe7983
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-22 11:46:01 -0400

    kangaroo2

---
+++ hackerrank/src/com/fabrizio/Kangaroo2.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class Kangaroo2 {
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(v1 <= v2){
+            return "NO";
+            } if(x2 - x1) % (v2 - v1) == 0 ? "YES" : "NO";
+        }
+    }
+
+}
