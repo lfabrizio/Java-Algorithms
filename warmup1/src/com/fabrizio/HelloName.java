0dd93328595d5082341ec458d43e8ad86a09532e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-22 12:54:14 -0400

    hello

---
+++ warmup1/src/com/fabrizio/HelloName.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+// print hello and then you name but on a new line
+public class HelloName {
+    public static void main(String[] args){
+        System.out.println("Hello\n Lou!");
+    }
+
+}
