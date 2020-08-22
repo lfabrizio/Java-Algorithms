03fc453de2c195cda4e85b6c71129cbbdbd80115
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-22 10:45:46 -0400

    kangaroo started

--- warmup1/src/com/fabrizio/Kangaroo.java
+++ warmup1/src/com/fabrizio/Kangaroo.java
@@ -3,4 +3,6 @@ package com.fabrizio;
 public class Kangaroo {
 
     // hackerrank kangaroo algorithm
+    // one kangarro jumps one. thr other jumps two
+    // try to find where they are on same spot at the same time
 }
