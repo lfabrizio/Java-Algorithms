2c7faf5ddfc4dbbac0acf71ed9db07d982c82450
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-24 19:09:16 -0400

    pattern

---
+++ warmup1/src/com/fabrizio/Pattern.java
@@ -0,0 +1,12 @@
+package com.fabrizio;
+
+public class Pattern {
+    public static void main(String[] args){
+        System.out.println(" ]   a   v     v    a");
+        System.out.println(" ]    aa   v    v   aa");
+        System.out.println(" J  J  aaaa  VV   aaaa");
+        System.out.println(" J   J  a  a  V  a   a");
+
+    }
+
+}
