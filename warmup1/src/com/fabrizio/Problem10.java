10be7bbafd02cf80d270c35ec335b982cb81c070
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-25 19:58:05 -0400

    problem 10

---
+++ warmup1/src/com/fabrizio/Problem10.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+
+public class Problem10 {
+    public static void main(String[] args){
+        Double Pi = 4.0*(1-(1.0/3)+(10/5);
+        System.out.println(Pi);
+    }
+}
--- warmup1/src/com/fabrizio/SpecifiedExpression.java
+++ warmup1/src/com/fabrizio/SpecifiedExpression.java
@@ -3,6 +3,7 @@ package com.fabrizio;
 public class SpecifiedExpression {
 
     public static void main(String[] args){
+
         System.out.println((25.5*3.5-3.5*35)/(40.5-4.5));
     }
 
