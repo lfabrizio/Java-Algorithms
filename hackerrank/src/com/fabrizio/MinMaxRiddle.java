c547533989f9ce7a977a8fca56328959dbf3dad0
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-25 19:38:39 -0400

    attempt at max min riddle

--- hackerrank/src/com/fabrizio/MinMaxRiddle.java
+++ hackerrank/src/com/fabrizio/MinMaxRiddle.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MinMaxRiddle {
-    static long[] riddle(long[] arr) {
+    static void riddle(long[] arr) {
         // complete this function
-        List<Long> result= new ArrayList<Long>(); //Creating arrayList
+        List<Long> result = new ArrayList<Long>(); //Creating arrayList
         Object[] array = result.toArray();
         int arrLength = arr.length;
 
-        for(int )
+        for (int idx = 0, idx2 = 1; idx < arrLength; idx++, idx2++) {
+            System.out.println(Math.min(arr[idx], arr[idx2]));
+        }
     }
-
 }
