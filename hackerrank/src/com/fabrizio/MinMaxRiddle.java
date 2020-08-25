141ac5ba2f789763723e38df934b9caf6a20636c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-25 19:26:26 -0400

    attempt at max min riddle

---
+++ hackerrank/src/com/fabrizio/MinMaxRiddle.java
@@ -0,0 +1,16 @@
+package com.fabrizio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinMaxRiddle {
+    static long[] riddle(long[] arr) {
+        // complete this function
+        List<Long> result= new ArrayList<Long>(); //Creating arrayList
+        Object[] array = result.toArray();
+        int arrLength = arr.length;
+
+        for(int )
+    }
+
+}
