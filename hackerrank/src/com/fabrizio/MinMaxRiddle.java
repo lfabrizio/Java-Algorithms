1285374a285198febc0392bb8ea4a30b12ab8a06
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-25 20:43:14 -0400

    add

--- hackerrank/src/com/fabrizio/MinMaxRiddle.java
+++ hackerrank/src/com/fabrizio/MinMaxRiddle.java
@@ -7,11 +7,45 @@ public class MinMaxRiddle {
     static void riddle(long[] arr) {
         // complete this function
         List<Long> result = new ArrayList<Long>(); //Creating arrayList
-        Object[] array = result.toArray();
-        int arrLength = arr.length;
-
+        Object[] array = result.toArray(); // to array
+        int arrLength = arr.length; //get array length
+        // loop through to find max and min
         for (int idx = 0, idx2 = 1; idx < arrLength; idx++, idx2++) {
             System.out.println(Math.min(arr[idx], arr[idx2]));
+
         }
     }
 }
+//
+//    static long[] riddle(long[] arr) {
+//        // complete this function
+//        List<Object[]> arrayList = new ArrayList<Object[]>();
+//        List<Long> longList = new ArrayList<Long>();
+//        long[] newArr = {1};
+//        int windowCounter = 1;
+//        for (int k = 0; k < windowCounter; k++) {
+//            for (int i = 0; i < arr.length; i++) {
+//                if (i + windowCounter > arr.length) {
+//                    break;
+//                }
+//                for (int j = i; j < windowCounter + i; j++) {
+//                    // System.out.println("Arr item: " + arr[j] + " J:" + j);
+//                    longList.add(arr[j]);
+//                }
+//                arrayList.add(longList.toArray());
+//                longList = new ArrayList<Long>();
+//            }
+//            if (windowCounter > arr.length) {
+//                break;
+//            } else {
+//                windowCounter++;
+//            }
+//        }
+//        for (Object[] longIntArray : arrayList) {
+//            for (Object longInt : longIntArray) {
+//                System.out.println(longInt);
+//            }
+//            System.out.println("New Array");
+//        }
+//        return newArr;
+//    }
--- warmup1/src/com/fabrizio/Problem10.java
+++ warmup1/src/com/fabrizio/Problem10.java
@@ -2,7 +2,7 @@ package com.fabrizio;
 
 public class Problem10 {
     public static void main(String[] args){
-        Double Pi = 4.0*(1-(1.0/3)+(10/5);
-        System.out.println(Pi);
+        double pi = 4.0*(1-(1.0/3)+(10/5));
+        System.out.println(pi);
     }
 }
