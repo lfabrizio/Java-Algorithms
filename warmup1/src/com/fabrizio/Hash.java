085dad68f0292cfa559ed4f85898440a3cb8f897
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-27 18:50:44 -0400

    perimeter

--- warmup1/src/com/fabrizio/Hash.java
+++ warmup1/src/com/fabrizio/Hash.java
@@ -1,34 +1,34 @@
-static int luckBalance(int k, int[][] contests) {
-        System.out.println(k);
-        List<Integer> luck = new ArrayList<Integer>();
-        int sum = 0;
-        for (int[] contest : contests) {
-        int i = contest[0];
-        int j = contest[1];
-        System.out.println(i + " " + j);
-        if (j == 1) {
-        luck.add(i);
-        }
-        sum += i;
-        }
-        System.out.println("Sum: " + sum);
-        System.out.println("Luck items");
-        Collections.sort(luck);
-        for (int luckItem : luck) {
-        System.out.println(luckItem);
-        }
-        for (int i = k; i > 0; i--) {
-        luck.remove(i);
-        }
-        System.out.println("After removal");
-        for (int luckItem : luck) {
-        System.out.println(luckItem);
-        }
-        for (int luckItem : luck) {
-        sum = sum - (luckItem * 2);
-        }
-        System.out.println("New sum: " + sum);
-        return sum;
-        }
+//static int luckBalance(int k, int[][] contests) {
+//        System.out.println(k);
+//        List<Integer> luck = new ArrayList<Integer>();
+//        int sum = 0;
+//        for (int[] contest : contests) {
+//        int i = contest[0];
+//        int j = contest[1];
+//        System.out.println(i + " " + j);
+//        if (j == 1) {
+//        luck.add(i);
+//        }
+//        sum += i;
+//        }
+//        System.out.println("Sum: " + sum);
+//        System.out.println("Luck items");
+//        Collections.sort(luck);
+//        for (int luckItem : luck) {
+//        System.out.println(luckItem);
+//        }
+//        for (int i = k; i > 0; i--) {
+//        luck.remove(i);
+//        }
+//        System.out.println("After removal");
+//        for (int luckItem : luck) {
+//        System.out.println(luckItem);
+//        }
+//        for (int luckItem : luck) {
+//        sum = sum - (luckItem * 2);
+//        }
+//        System.out.println("New sum: " + sum);
+//        return sum;
+//        }
 
 
--- warmup1/src/com/fabrizio/icecream.java
+++ warmup1/src/com/fabrizio/icecream.java
@@ -1,24 +1,24 @@
-package com.fabrizio;
-
-public class icecream {
-
-    // Complete the whatFlavors function below.
-    static void whatFlavors(int[] cost, int money) {
-        // array we will return
-        int[] tempArray = new int[2];
-
-        // nested loop
-        for (int i = 0; i < cost.length; i++) {
-            //inner loop
-            for (int j = i + 1; j < cost.length; j++) {
-                //if statement to determine if combo equals the $
-                if (cost[i] + cost[j] == money) {
-                    // place indexes in temp array
-                    tempArray[0] = i+1;
-                     tempArray[1] = j+1;
-                    return tempArray;
-                }
-            }
-
-        }
-    }}
+//package com.fabrizio;
+//
+//public class icecream {
+//
+//    // Complete the whatFlavors function below.
+//    static void whatFlavors(int[] cost, int money) {
+//        // array we will return
+//        int[] tempArray = new int[2];
+//
+//        // nested loop
+//        for (int i = 0; i < cost.length; i++) {
+//            //inner loop
+//            for (int j = i + 1; j < cost.length; j++) {
+//                //if statement to determine if combo equals the $
+//                if (cost[i] + cost[j] == money) {
+//                    // place indexes in temp array
+//                    tempArray[0] = i+1;
+//                     tempArray[1] = j+1;
+//                    return tempArray;
+//                }
+//            }
+//
+//        }
+//    }}
