7274dc50353f86ae0623432fac22bea729f4c944
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-29 11:36:09 -0400

    compare two numbers

--- warmup2/src/com/fabrizio/Main.java
+++ warmup2/src/com/fabrizio/Main.java
@@ -1,8 +1,33 @@
 package com.fabrizio;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write to compare two numbers
+        // create scanner
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+        System.out.print("Input first Number: ");
+        number1 = input.nextInt(); //read number from user and input in variable number1
+
+        System.out.print("Input Second number: ");
+        number2 = input.nextInt(); //same but for variable number2
+
+
+        if ( number1 == number2 )
+            System.out.printf( "%d == %d\n", number1, number2 );
+        if ( number1 != number2 )
+            System.out.printf( "%d != %d\n", number1, number2 );
+        if ( number1 < number2 )
+            System.out.printf( "%d < %d\n", number1, number2 );
+        if ( number1 > number2 )
+            System.out.printf( "%d > %d\n", number1, number2 );
+        if ( number1 <= number2 )
+            System.out.printf( "%d <= %d\n", number1, number2 );
+        if ( number1 >= number2 )
+            System.out.printf( "%d >= %d\n", number1, number2 );
     }
 }
