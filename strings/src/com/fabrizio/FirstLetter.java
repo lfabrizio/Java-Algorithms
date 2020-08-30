be10e597bc444af536db3af2d2439bfc60b43e03
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-30 10:22:46 -0400

    uppercase first letter only

---
+++ strings/src/com/fabrizio/FirstLetter.java
@@ -0,0 +1,23 @@
+package com.fabrizio;
+
+public class FirstLetter {
+    //first letter to uppercase only
+    // create new string object
+    public static void main(String[] args){
+    String x = "sample word";
+    char y[] = x.toCharArray();
+    int size = y.length;
+
+    y[0] = (char)(y[0]-32);
+    int i = 1;
+    while(i != size)
+    {
+        if(y[i] == ' ')
+        {
+            y[i+1] = (char) (y[i+1]-32);
+        }
+        ++i;
+    }
+    System.out.println(x);
+    System.out.println(y);
+}}
