8e014e76c9c5c9fcac5de41ee4ea992d60341ab0
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-08-30 17:34:38 -0400

    copy a string

---
+++ strings/src/com/fabrizio/CopyString.java
@@ -0,0 +1,19 @@
+package com.fabrizio;
+
+public class CopyString {
+
+    public static void main(String [] args){
+        String x = "ABC Test";
+        char y[] = x.toCharArray();
+        int size = y.length;
+        char a[] = new char[size];
+        int i = 0;
+        while(i != size)
+        {
+            a[i] = y[i];
+            ++i;
+        }
+        System.out.println(y);
+        System.out.println(a);
+    }
+}
