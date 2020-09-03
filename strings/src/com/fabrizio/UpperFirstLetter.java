9adad6113d4ea232e2aea9e1c7a73c900f9f16e8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-02 21:18:16 -0400

    spaces

---
+++ strings/src/com/fabrizio/UpperFirstLetter.java
@@ -0,0 +1,17 @@
+package com.fabrizio;
+
+public class UpperFirstLetter {
+    public static void main(String[] args){
+        // uppercase only first letter of string
+        String x = "uppercase all letters";
+        char y[] = x.toCharArray();
+        int size = y.length;
+
+        y[0] = (char) (y[0]-32); //set to uppercase - first overall character
+
+        int i = 1;
+        if(i == ' '){ // if statement to check if space white space
+
+        }
+    }
+}
