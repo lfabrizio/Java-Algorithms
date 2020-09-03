99e8c7984088b9d3ff426b78af492ea43ac190fe
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-03 18:03:49 -0400

    first and space

--- strings/src/com/fabrizio/UpperFirstLetter.java
+++ strings/src/com/fabrizio/UpperFirstLetter.java
@@ -1,17 +1,23 @@
 package com.fabrizio;
 
 public class UpperFirstLetter {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // uppercase only first letter of string
         String x = "uppercase all letters";
         char y[] = x.toCharArray();
         int size = y.length;
 
-        y[0] = (char) (y[0]-32); //set to uppercase - first overall character
+        y[0] = (char) (y[0] - 32); //set to uppercase - first overall character
 
         int i = 1;
-        if(i == ' '){ // if statement to check if space white space
-            y[i +1] == y[i] -32;
+        while (i != size) {
+            if (y[i] == ' ')
+            { // if statement to check if space white space
+                y[i + 1] = (char) (y[i+1] - 32);
+            }
+            ++i;
         }
+        System.out.println(x);
+        System.out.println(y);
     }
 }
