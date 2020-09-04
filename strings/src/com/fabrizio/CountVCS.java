45fab7929cebe95918b192f39129d4615b929889
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-03 20:17:20 -0400

    vowel or special

---
+++ strings/src/com/fabrizio/CountVCS.java
@@ -0,0 +1,12 @@
+package com.fabrizio;
+// see if constants, vowels or special chr
+public class CountVCS {
+    public static void main(String[] args){
+        String x = "Count the variables and char";
+        char y[] = x.toCharArray();
+        int size = y.length;
+        int vowels = 0;
+        int consts = 0;
+        int special = 0;
+    }
+}
