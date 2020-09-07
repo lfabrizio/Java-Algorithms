6990e340fce3bcb7eafd8238b58b0d99a4d8eaf8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-07 17:18:09 -0400

    pangram

--- strings/src/com/fabrizio/Panagram.java
+++ strings/src/com/fabrizio/Panagram.java
@@ -6,7 +6,27 @@ public class Panagram {
     //loop over array to maek sure every alphabet
     public static void main(String[] args){
         String x = "the quick brown fox jumps over lazy dog";
+        x = x.replace(" ", "");
+        char y[] = x.toCharArray();
+        int size = y.length;
+        int a[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
+        int i = 0;
+        while(i != size){
+            int index = y[i]-65;
+            a[index] = 1;
+            ++i;
+        }
+        i = 0;
+        while(i != 26){
+            if(a[i] == 1){
+                ++i;
+            }
+            else{
+                System.out.println("non-pangram");
+            }
+        }
+        System.out.println("pangram");
     }
 
 }
