86ba49748408323dbd3ad78c3e12a3ed03145333
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-07 11:15:30 -0400

    anagrma

--- strings/src/com/fabrizio/Anagram.java
+++ strings/src/com/fabrizio/Anagram.java
@@ -1,5 +1,29 @@
 package com.fabrizio;
 
+import java.util.Arrays;
+
 public class Anagram {
     // what is an anagram?
+    public static void main(String[] args){
+        String x = "silent";
+        String y = "listen";
+        char a[] = x.toCharArray();
+        char b[] = y.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        Boolean result = Arrays.equals(a, b);
+
+        if(result == true)
+        {
+            System.out.println("this is an anagram");
+
+        }
+        else
+        {
+            System.out.println("not an anagram");
+        }
+
+    }
 }
