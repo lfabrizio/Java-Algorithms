54b65dc726548291ae1022c3d7ebdda30e50ff6a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-07 11:51:14 -0400

    pangram

--- strings/src/com/fabrizio/Panagram.java
+++ strings/src/com/fabrizio/Panagram.java
@@ -3,8 +3,9 @@ package com.fabrizio;
 public class Panagram {
 
     // start panagram algo
-    //loop over array to maek sure every alphabet 
+    //loop over array to maek sure every alphabet
     public static void main(String[] args){
+        String x = "the quick brown fox jumps over lazy dog";
 
     }
 
