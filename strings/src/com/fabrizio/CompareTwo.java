b30a2cc3aad840a042056f6f8f4aa6dc0301af2e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-10 19:08:59 -0400

    compare

--- strings/src/com/fabrizio/CompareTwo.java
+++ strings/src/com/fabrizio/CompareTwo.java
@@ -6,8 +6,18 @@ public class CompareTwo {
     //compare two
     public static void main(String[] args){
         // create scanner
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int numberOne;
         int numberTwo;
+
+        System.out.print( "Input first integer: " ); // prompt
+        numberOne = input.nextInt(); // read first number from user
+
+        System.out.print( "Input second integer: " ); // prompt
+        numberTwo = input.nextInt(); // read second number from user
+        
+        if(numberOne == numberOne){
+            System.out.println("equal numbers")
+        }
     }
 }
