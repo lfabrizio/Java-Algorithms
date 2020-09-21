366b6a0deb28946aade6ffd318ed37330e41475c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-21 17:43:16 -0400

    debit card

--- InClass/src/BankTools/DebitCard.java
+++ InClass/src/BankTools/DebitCard.java
@@ -1,4 +1,39 @@
 package BankTools;
 
+import BankAccount.CheckingAccount;
+import BankAccount.SavingsAccount;
+
+import java.util.*;
+
 public class DebitCard {
+    private String pin;
+    private CheckingAccount primary;
+    //    private Calendar expDate;
+    private String cvv;
+    private String cardOwner;
+    private String cardNum;
+    private List<SavingsAccount> atmAccounts;
+
+    public DebitCard(String cardNum, String pin, String cvv, String cardOwner, CheckingAccount primary, List atmAccounts) {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.cvv = cvv;
+        this.cardOwner = cardOwner;
+        this.primary = primary;
+        this.atmAccounts = atmAccounts;
+    }
+
+    public Boolean charge(int amt, String pin) {
+        if (this.pin == pin && primary.getBalance() >= amt) {
+            primary.withdraw(amt);
+            return true;
+        }
+        return false;
+    }
 }
+
+// create a Client class
+// 2 of your own properties
+// listArray of CheckingAccounts
+// listArray of SavingsAccounts
+// a method to add an account to the client (try to use overloading) addAccount()
