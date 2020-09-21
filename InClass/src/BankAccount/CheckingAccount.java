0b73c0887d6ee43bb87e6823438e8672834cbe4b
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-21 17:27:36 -0400

    checking account

--- InClass/src/BankAccount/CheckingAccount.java
+++ InClass/src/BankAccount/CheckingAccount.java
@@ -1,4 +1,7 @@
 package BankAccount;
 
-public class CheckingAccount {
+public class CheckingAccount extends BankAccount {
+    public CheckingAccount(int accountNum, int balance, String owner) {
+        super(accountNum, balance, owner, "Checking");
+    }
 }
