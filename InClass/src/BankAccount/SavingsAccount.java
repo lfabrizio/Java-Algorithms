fe8b33dfd899cf99836f558e86bcdb8989a6f61f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-21 17:41:00 -0400

    savings account

--- InClass/src/BankAccount/SavingsAccount.java
+++ InClass/src/BankAccount/SavingsAccount.java
@@ -1,4 +1,14 @@
 package BankAccount;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    private int interest;
+
+    public SavingsAccount (int accountNum, int balance, String owner, int interest) {
+        super(accountNum, balance, owner, "Savings");
+        this.interest = interest;
+    }
+
+    public void applyInterest() {
+        deposit( (int) (balance * ((float) interest / 100.0)));
+    }
 }
