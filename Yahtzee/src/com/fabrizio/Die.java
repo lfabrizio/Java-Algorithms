62ea4356c0486c851a630c0bf0a7e602098bb0ee
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-22 17:25:34 -0400

    yahtzee

--- Yahtzee/src/com/fabrizio/Die.java
+++ Yahtzee/src/com/fabrizio/Die.java
@@ -2,15 +2,20 @@ package com.fabrizio;
 
 import java.security.SecureRandom;
 import java.util.Random;
-
+//v1.0 I have a die
+// v1.1 that die can be rolled to have a random value
 public class Die {
+    // get the die
+    //also store number of sides of the die
     private int diceSides = 6; // dice sides
+    private int value = 0; //to store value of dice rolled
     private int timesRolled = 0; // keep track of how many times rolled
 
-    public void roll() {
-        Random random = new Random();
-        // +1  to stay away from zeros
-        random.nextInt((diceSides));// this isnt right.. working on it
-    }
+    public int roll() {
+        Random random = new Random(); //random
+
+       value =  random.nextInt((diceSides + 1)); // will give random number from zero to dicesides, so must add 1
+        return value;
     }
 }
+
