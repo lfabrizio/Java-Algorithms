54ada6d9aa689ee0ef306a3db811ff66de0797e1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-24 18:39:59 -0400

    continued

--- InClass/src/com/fabrizio/Yahtzee/Console.java
+++ InClass/src/com/fabrizio/Yahtzee/Console.java
@@ -7,13 +7,16 @@ public class Console {
     //user prompts
     Scanner in = new Scanner(System.in);
 
-    public void userInteraction() {
+
+
+    public void play() {
         System.out.println("Player 1, would you like to roll?"); //ask
         String user = in.nextLine(); // response
         if(user == "No" || user == "N"){
             break;
         } else {
     System.out.println("Which die would you like to re-roll? ");
+
         }
     }
 }
